07aeea96ea3dd332e69f81a817a1df1d7719d27a
Max Friedland <mef5530@rit.edu>
2022-03-06 23:46:01 -0500

    a

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import os
-import string
-import subprocess
-from time import sleep
+#!/usr/bin/python3
 
+#Max Friedland
+#3/4/22
+
+import os
+import subprocess
+
+#clears the terminal
 def cls():
     os.system("clear")
 
+#runs the command and returns the output in an array
 def command_subprocess(arg):
     process = subprocess.Popen(arg, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
     output = process.communicate()
@@ -16,12 +21,14 @@ def command_subprocess(arg):
     except IndexError:
         return []
 
+#checks if the arg is a symlink
 def check_symlink(arg):
     output = command_subprocess("readlink " + arg)
     if (len(output) == 1):
         return True
     return False
 
+#helps me format fancy text:)
 def text_format(arg):
     if arg == "default":
         print("\033[0;37;40m")
@@ -32,6 +39,7 @@ def text_format(arg):
     elif arg == "warning":
         print("\033[0;33;40m")
 
+#clears screen, displays welcome, shows working dir
 def gui_welcome_message():
     cls()
     text_format("good")
@@ -44,15 +52,17 @@ def gui_welcome_message():
     wd = command_subprocess("pwd")
     print("Your current working directory is: " + wd[0])
 
+#displays the main menu and returns the selection
 def gui_main_menu() -> str:
     text_format("default")
     print("Enter Selection:")
     print(" 1. \033[1;31;40mCreate\033[0;37;40m a shortcut in your home directory. ")
     print(" 2. \033[1;31;40mRemove\033[0;37;40m a shortcut in your home directory.")
     print(" 3. \033[1;31;40mRun shortcut report.")
-    text_format("warning"); print("\nPlease enter a number (1-3) or (q/Q) to quit\n"); text_format("default")
+    text_format("warning"); print("\nPlease enter a number (1-3) or (quit) to quit\n"); text_format("default")
     return input(">>> ")
 
+#helper to create symlinks (run when user selects 1)
 def run_create_sym():
     text_format("warning"); arg = input("Please enter the filename to create a shortcut >>> \033[0;37;40m");
     output:list = command_subprocess("find $HOME -name " + arg)
@@ -66,6 +76,12 @@ def run_create_sym():
             text_format("critical"); print("No files found, returning to menu...")
         elif (len(output) == 1):
             print("Found one file, " + output)
+            lnoutput = command_subprocess("ln -s " + output[0] + " $HOME/Desktop/" + arg)
+            if ("File exists" in lnoutput[0]):
+                text_format("critical");
+                print("Task failed, file already exists.")
+            else:
+                print("Task done!")
         else:
             print("Found multiple files:")
             for i in range(0, len(output)):
@@ -74,11 +90,22 @@ def run_create_sym():
             lnoutput = command_subprocess("ln -s " + output[selection] + " $HOME/Desktop/" + arg)
             if("File exists" in lnoutput[0]):
                 text_format("critical"); print("Task failed, file already exists.")
+            else:
+                print("Task done!")
     else:
-        text_format("critical"); print("file already exists")
-def run_remove_sym():
-    pass
+        text_format("critical"); print("File already exists")
 
+#helper to remove symlinks (selection is 2)
+def run_remove_sym():
+    fn = input("Enter the name of the link to remove >>> ")
+    if check_symlink("/$HOME/Desktop/" + fn):
+        resp = input("Are you sure you want to remove " + fn + " (y/N")
+        if (resp.lower() == "y"):
+            command_subprocess("rm -rf /$HOME/Desktop/" + fn)
+    else:
+        print("That isnt a symlink...")
+
+#helper to run report (selection is 3)
 def run_report():
     symlinkcount = 0
     desktopitems = command_subprocess("ls /$HOME/Desktop")
@@ -92,9 +119,10 @@ def run_report():
         if check_symlink("/$HOME/Desktop/" + e):
             print(e + "   :  " + command_subprocess("readlink /$HOME/Desktop/" + e)[0])
 
+#driver for the main menu
 def main_menu():
-    gui_welcome_message()
     while(True):
+        gui_welcome_message()
         text_format("default")
         output:str = gui_main_menu()
         if output == "1":
@@ -103,7 +131,7 @@ def main_menu():
             run_remove_sym()
         elif output == "3":
             run_report()
-        elif output.lower() == "q":
+        elif output.lower() == "quit":
             text_format("critical"); print("Goodbye!"); text_format("default")
             break
         else:
